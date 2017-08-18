c3d2312a17c6e393322f559fdb87914aaeb0fba3
El-kabir Moutuou <elmoutuou@itool.com>
2017-08-18 17:19:14 +0200

    commit 3

---
+++ PerformanceCounter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Diagnostics;
+namespace PerformanceCounter
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(!PerformanceCounterCategory.Exist("FourthCoffeddOrders"))
+            {
+            	
+            }
+        }
+
+
+    }
+}
